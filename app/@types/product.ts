32dadc75e7c43e383955cfcc1e2c712bf61d2645
Roman Tuomisto <romantuomisto@gmail.com>
2025-12-18 09:42:53 +0200

    adds product types

--- app/@types/product.ts
+++ app/@types/product.ts
@@ -1,21 +1,32 @@
-import type { Category } from './category'
-import type { Genre } from './genre'
+import type { Category } from "./category";
+import type { Genre } from "./genre";
 
-export type Format = 'lp' | "7'" | 'cd' | "12'" | 'kasetti' | 'lahjakortti' | 'palvelu'
+export type Format =
+  | "lp"
+  | "7'"
+  | "cd"
+  | "12'"
+  | "kasetti"
+  | "lahjakortti"
+  | "palvelu";
 
 export interface Product {
-  _id: string
-  title: string
-  name: string
-  edition?: string
-  fullname: string
-  description?: string
-  cover: string
-  unit_price: number
-  year?: number
-  label?: string
-  format: Format
-  category: Category
-  genre: Genre
-  tracklist?: string[]
+  _id: string;
+  title: string;
+  name: string;
+  edition?: string;
+  fullname: string;
+  description?: string;
+  cover: string;
+  unit_price: number;
+  year?: number;
+  label?: string;
+  format: Format;
+  category: Category;
+  genre: Genre;
+  tracklist?: string[];
+}
+
+export interface ProductResponse {
+  products: Product[];
 }
